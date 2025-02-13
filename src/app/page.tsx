7261781c7fd59c60b1337b2ce3fc724cd4649bc1
caerroff <56608891+caerroff@users.noreply.github.com>
2025-02-13 13:25:18 +0200

    fix(images): add pathname

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import Base from "./components/Base";
+import { usePathname } from "next/navigation";
 
 export default function Home() {
+  const pathname = usePathname()
+
   return (
     <>
       <Base>
         <div className="relative">
-          <Image src={"/solvay.jpg"} height={1500} width={1500} objectFit="cover" alt="Solvay Conference" />
+          <Image src={pathname + "/solvay.jpg"} height={1500} width={1500} objectFit="cover" alt="Solvay Conference" />
           <div className="absolute bottom-24 left-0 right-0 text-center text-white text-shadow bg-slate-500 bg-opacity-20">
             <h1 className="text-7xl font-bold">Octopus Alliances</h1>
             <p className="text-2xl font-bold">It&apos;s all about you</p>
@@ -27,7 +30,7 @@ export default function Home() {
             <div className="w-1/2"></div>
             <div className="w-1/2">
               <div className="flex space-x-5 justify-center">
-                <Image src={"/octopus.png"} height={150} width={150} alt="Octopus Logo" className="place-self-center rounded-2xl" />
+                <Image src={pathname + "/octopus.png"} height={150} width={150} alt="Octopus Logo" className="place-self-center rounded-2xl" />
                 <p className="font-bold text-3xl self-center font-serif">Occy Social Alliances</p>
               </div>
               <p>Occy Social Alliances connect you to a dynamic global commerce ecosystem where people, businesses, and communities unite to inspire growth and success. With a focus on building meaningful alliances Occy empowers you to thrive</p>
