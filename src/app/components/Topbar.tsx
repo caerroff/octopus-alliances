f503fdfe0b1c948b72335c1d60c65d41314f7e26
caerroff <56608891+caerroff@users.noreply.github.com>
2025-02-13 13:33:06 +0200

    fix(pathname)

--- src/app/components/Topbar.tsx
+++ src/app/components/Topbar.tsx
@@ -7,12 +7,12 @@ export default function Topbar() {
     return (
         <>
             <div className="flex justify-between bg-primary min-h-10 place-items-center">
-                <Image height={50} width={50} src={pathname + "/octopus.png"} alt="Octopus Logo" className="m-2 object-cover rounded-2xl" />
+                <Image height={50} width={50} src={(pathname == '/' ? "" : pathname) + "/octopus.png"} alt="Octopus Logo" className="m-2 object-cover rounded-2xl" />
                 <div className="flex flex-col justify-center text-orange-300 text-2xl text-center font-bold">
                    <p>Octopus Alliances</p>
                    <p className="text-lg">It&apos;s all about you</p>
                 </div>
-                <Image height={50} width={50} src={pathname + "/octopus.png"} alt="Octopus Logo" className="m-2 object-cover rounded-2xl" />
+                <Image height={50} width={50} src={(pathname == '/' ? "" : pathname) + "/octopus.png"} alt="Octopus Logo" className="m-2 object-cover rounded-2xl" />
             </div>
         </>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <>
       <Base>
         <div className="relative">
-          <Image src={pathname + "/solvay.jpg"} height={1500} width={1500} objectFit="cover" alt="Solvay Conference" />
+          <Image src={(pathname == '/' ? "" : pathname) + "/solvay.jpg"} height={1500} width={1500} objectFit="cover" alt="Solvay Conference" />
           <div className="absolute bottom-24 left-0 right-0 text-center text-white text-shadow bg-slate-500 bg-opacity-20">
             <h1 className="text-7xl font-bold">Octopus Alliances</h1>
             <p className="text-2xl font-bold">It&apos;s all about you</p>
@@ -31,7 +31,7 @@ export default function Home() {
             <div className="w-1/2"></div>
             <div className="w-1/2">
               <div className="flex space-x-5 justify-center">
-                <Image src={pathname + "/octopus.png"} height={150} width={150} alt="Octopus Logo" className="place-self-center rounded-2xl" />
+                <Image src={(pathname == '/' ? "" : pathname) + "/octopus.png"} height={150} width={150} alt="Octopus Logo" className="place-self-center rounded-2xl" />
                 <p className="font-bold text-3xl self-center font-serif">Occy Social Alliances</p>
               </div>
               <p>Occy Social Alliances connect you to a dynamic global commerce ecosystem where people, businesses, and communities unite to inspire growth and success. With a focus on building meaningful alliances Occy empowers you to thrive</p>
@@ -41,7 +41,7 @@ export default function Home() {
           <div className="flex pt-10">
             <div className="w-1/2">
               <div className="flex space-x-5 justify-center">
-                <Image src={pathname + "/octopus.png"} height={150} width={150} alt="Octopus Logo" className="place-self-center rounded-2xl" />
+                <Image src={(pathname == '/' ? "" : pathname) + "/octopus.png"} height={150} width={150} alt="Octopus Logo" className="place-self-center rounded-2xl" />
                 <p className="font-bold text-3xl self-center font-serif">Occy Business Alliances</p>
               </div>
               <h3 className="font-bold text-2xl py-5">Directories</h3>
