e31b3b78b8546bbe9634e5d4fbc0c37df766597a
Caerroff <caerroff.dev@gmail.com>
2025-02-26 14:08:22 +0200

    fix(ts): change rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,16 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities" : "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@typescript-eslint/no-empty-object-type": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "allowObjectTypes" : "off"
+    }
+  }
 ];
 
 export default eslintConfig;
